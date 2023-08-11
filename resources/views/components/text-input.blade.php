33b7f4b69db39174711c5a0a933d34b1109d4b14
Gandobilis <lashadeveloper@gmail.com>
2023-08-11 14:44:43 +0400

    breeze for blade has been initialized.

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,5 +1,3 @@
 @props(['disabled' => false])
 
-<label>
-    <input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}>
-</label>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}>
