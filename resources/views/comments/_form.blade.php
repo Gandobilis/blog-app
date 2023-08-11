0f52962aa17c2dceec1e25e773a771595683573c
Gandobilis <lashadeveloper@gmail.com>
2023-08-11 14:29:22 +0400

    breeze for blade has been initialized.

--- resources/views/comments/_form.blade.php
+++
@@ -1,8 +0,0 @@
-<form action="{{ route('comments.store') }}" method="post">
-    @csrf
-    <input type="hidden" name="post_id" value="{{ $post->id }}">
-    <label>
-        <textarea name="content" rows="3"></textarea>
-    </label>
-    <button type="submit" class="btn btn-primary">Add Comment</button>
-</form>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\CommentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +14,18 @@ use App\Http\Controllers\CommentController;
 |
 */
 
-Route::get('/', [PostController::class, 'index']);
-Route::resource('posts', PostController::class);
-Route::resource('comments', CommentController::class)->only('store');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
