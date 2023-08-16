29d1aa6417718af0ee0125fc0770399874966f8c
Gandobilis <lashadeveloper@gmail.com>
2023-08-16 18:06:42 +0400

    Comment Like Logic implemented

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\Comment\CommentRequest;
 use App\Models\Comment;
 use App\Models\Post;
+use App\Models\CommentLike;
 
 class CommentController extends Controller
 {
@@ -25,18 +26,27 @@ class CommentController extends Controller
         $newData = [
             'content' => $request->validated('content')
         ];
-        if ($comment->user_id === auth()->id()) {
+        if ($comment->user_id === auth()->id())
             $comment->update($newData);
-        }
 
         return back();
     }
 
     public function destroy(Comment $comment)
     {
-        if ($comment->user_id === auth()->id()) {
+        if ($comment->user_id === auth()->id())
             $comment->delete();
-        }
+
+        return back();
+    }
+
+    public function toggleLike(Comment $comment)
+    {
+
+        if (!$comment->likes->contains(auth()->id()))
+            $comment->likes()->attach(auth()->id());
+        else
+            $comment->likes()->detach(auth()->id());
 
         return back();
     }
--- resources/views/components/comment.blade.php
+++ resources/views/components/comment.blade.php
@@ -20,18 +20,31 @@
         </div>
     </form>
     @if(auth()->id() === $comment->user_id)
-        <div x-show="!edit">
-            <x-secondary-button @click="edit = true">
-                <i class="fa-solid fa-pen"></i>
-            </x-secondary-button>
-            <form method="POST" class="inline" action="{{ route('comment.destroy', $comment) }}">
-                @csrf
-                @method('DELETE')
+        <div x-show="!edit" class="flex items-center space-x-5">
+            <div>
+                <x-secondary-button @click="edit = true">
+                    <i class="fa-solid fa-pen"></i>
+                </x-secondary-button>
+                <form method="POST" class="inline" action="{{ route('comment.destroy', $comment) }}">
+                    @csrf
+                    @method('DELETE')
 
-                <x-primary-button type="submit">
-                    <i class="fa-solid fa-trash-can"></i>
-                </x-primary-button>
-            </form>
+                    <x-primary-button type="submit">
+                        <i class="fa-solid fa-trash-can"></i>
+                    </x-primary-button>
+                </form>
+            </div>
         </div>
     @endif
+    <div class="flex items-center gap-x-2">
+        @if(auth()->id() !== $comment->user_id)
+        <form class="inline" action="{{ route('comments.toggleLike', $comment) }}" method="POST">
+            @csrf
+            <button type="submit">
+                <i class="fa-solid fa-thumbs-up text-blue-700"></i>
+            </button>
+        </form>
+        @endif
+        <p>{{$comment->likes->count()}} Likes</p>
+    </div>
 </div>
